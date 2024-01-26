f7cd3036ffbeeece76211794dbce2a7e2740c14b
James <jamesbhaney53@gmail.com>
2024-01-25 18:03:51 -0600

    configured pathplanner

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,6 +1,11 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -10,6 +15,7 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -76,7 +82,33 @@ public class Drivetrain extends SubsystemBase {
     m_snapToRotationController.setSetpoint(0);
 
     m_inHighGear = m_config.Drivetrain.StartInHighGear;
-  }
+
+        AutoBuilder.configureHolonomic(
+                this::getPose, // Robot pose supplier
+                this::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+                this::getChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                this::drive, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+                        new PIDConstants(0, 0, 0), // Translation PID constants
+                        new PIDConstants(0, 0, 0), // Rotation PID constants
+                        4.5, // Max module speed, in m/s
+                        0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                        new ReplanningConfig() // Default path replanning config. See the API for the options here
+                ),
+                () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
+                this // Reference to this subsystem to set requirements
+        );
+    }
 
   /**
    * Creates the swerve modules and starts odometry
@@ -311,7 +343,15 @@ public class Drivetrain extends SubsystemBase {
     m_snapToGyroEnabled = !m_snapToGyroEnabled;
     m_snapToRotationController.setSetpoint(0);
   }
-
+  
+  public ChassisSpeeds getChassisSpeeds(){
+    return m_kinematics.toChassisSpeeds(
+      m_frontLeftModule.getModuleState(),
+      m_rearLeftModule.getModuleState(),
+      m_rearRightModule.getModuleState(),
+      m_frontRightModule.getModuleState(),
+      )
+  }
   /**
    * Updates odometry and any other periodic drivetrain events
    */
